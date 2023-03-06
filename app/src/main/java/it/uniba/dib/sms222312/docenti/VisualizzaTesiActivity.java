e63a1d76a2c4075a85e96daddfa71bb42196b39b
Nicola De Veteris <n.deveteris@studenti.uniba.it>
2023-03-06 15:36:20 +0100

    Fatta la condivisione del qr code sia su studente che su docente

--- app/src/main/java/it/uniba/dib/sms222312/docenti/VisualizzaTesiActivity.java
+++ app/src/main/java/it/uniba/dib/sms222312/docenti/VisualizzaTesiActivity.java
@@ -2,10 +2,16 @@ package it.uniba.dib.sms222312.docenti;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,8 +24,11 @@ import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import it.uniba.dib.sms222312.R;
+import it.uniba.dib.sms222312.studenti.VisualizzaTesiStudenteActivity;
 
 public class VisualizzaTesiActivity extends AppCompatActivity {
+
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,13 +47,14 @@ public class VisualizzaTesiActivity extends AppCompatActivity {
         TextView textDurata = findViewById(R.id.durata);
         ImageView imageQr = findViewById(R.id.qr);
         Button modificaButton = findViewById(R.id.modifica);
+        Button condividiButton = findViewById(R.id.condividi);
 
         String text ="Nome: " + nome + " Corso: " + corso + " Media: " + media + " Durata: " + durata + " ore Descrizione: " + descrizione;
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         try {
             BitMatrix matrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 400, 400);
             BarcodeEncoder encoder = new BarcodeEncoder();
-            Bitmap bitmap = encoder.createBitmap(matrix);
+            bitmap = encoder.createBitmap(matrix);
             imageQr.setImageBitmap(bitmap);
         } catch (WriterException e) {
             throw new RuntimeException(e);
@@ -67,6 +77,28 @@ public class VisualizzaTesiActivity extends AppCompatActivity {
                 intent.putExtra("Durata", durata);
 
                 startActivity(intent);
+            }
+        });
+
+        condividiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(VisualizzaTesiActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(VisualizzaTesiActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                }else{
+                    // Codice per condividere un Bitmap su WhatsApp
+                    String bitmapPath = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Shared image", null);
+
+                    Uri bitmapUri = Uri.parse(bitmapPath);
+
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("image/*");
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, bitmapUri);
+                    //shareIntent.setPackage("com.whatsapp");
+
+                    startActivity(Intent.createChooser(shareIntent, "Condividi immagine"));
+                }
+
             }
         });
 
--- app/src/main/java/it/uniba/dib/sms222312/studenti/VisualizzaTesiStudenteActivity.java
+++ app/src/main/java/it/uniba/dib/sms222312/studenti/VisualizzaTesiStudenteActivity.java
@@ -2,10 +2,16 @@ package it.uniba.dib.sms222312.studenti;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -35,6 +41,8 @@ import it.uniba.dib.sms222312.modelli.Classifica;
 
 public class VisualizzaTesiStudenteActivity extends AppCompatActivity {
 
+    Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,13 +64,14 @@ public class VisualizzaTesiStudenteActivity extends AppCompatActivity {
         ImageView imageQr = findViewById(R.id.qr);
         Button classificaButton = findViewById(R.id.classifica);
         Button richiediButton = findViewById(R.id.richiesta);
+        Button condividiButton = findViewById(R.id.condividi);
 
         String text ="Nome: " + nome + " Corso: " + corso + " Media: " + media + " Durata: " + durata + " ore Descrizione: " + descrizione;
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
         try {
             BitMatrix matrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 400, 400);
             BarcodeEncoder encoder = new BarcodeEncoder();
-            Bitmap bitmap = encoder.createBitmap(matrix);
+            bitmap = encoder.createBitmap(matrix);
             imageQr.setImageBitmap(bitmap);
         } catch (WriterException e) {
             throw new RuntimeException(e);
@@ -136,6 +145,28 @@ public class VisualizzaTesiStudenteActivity extends AppCompatActivity {
                         }
                     }
                 });
+            }
+        });
+
+        condividiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(VisualizzaTesiStudenteActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(VisualizzaTesiStudenteActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                }else{
+                    // Codice per condividere un Bitmap su WhatsApp
+                    String bitmapPath = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Shared image", null);
+
+                    Uri bitmapUri = Uri.parse(bitmapPath);
+
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("image/*");
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, bitmapUri);
+                    //shareIntent.setPackage("com.whatsapp");
+
+                    startActivity(Intent.createChooser(shareIntent, "Condividi immagine"));
+                }
+
             }
         });
     }
